83b6e314ab29cb0d7dce6b26b178b6b6cc0e0963
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-23 13:33:41 +0100

    feat: create application codes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const debug = require('debug')('app');
+const config = require('./config')
 const { PORT } = require('./config');
 
 const app = express();
@@ -20,7 +21,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/', vendor);
 
 if (process.env.NODE_ENV === 'production') {
-  if(!process.env){
+  if(!config){
     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`)
     process.exist(1)
   }
