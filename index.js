ee845558b2b02a7f12631442d66aafe2316ec5bc
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-24 13:28:47 +0100

    feat: change environment variables

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use('/', vendor);
 
+
+// console.log(process.env)
 if (process.env.NODE_ENV === 'production') {
-  if (!process.env.debug) {
+  if (!process.env) {
     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
     process.exist(1);
   }
