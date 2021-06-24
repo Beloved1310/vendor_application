a122c1aeb553e7e5a0bb66e8c5620fc4ec84bdb4
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-24 15:12:38 +0100

    feat: set production function

--- index.js
+++ index.js
@@ -21,41 +21,6 @@ app.use('/', vendor);
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   if (!process.env.CLOUDINARY_CLOUD_NAME ) {
-//     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-//     process.exist(1);
-//   }
-// }
-
-// if (process.env.NODE_ENV === 'production') {
-//   if (!process.env.debug ) {
-//     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-//     process.exist(1);
-//   }
-// }
-
-// if (process.env.NODE_ENV === 'production') {
-//   if (!process.env.MONGODBURI ) {
-//     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-//     process.exist(1);
-//   }
-// }
-
-// if (process.env.NODE_ENV === 'production') {
-//   if (!process.env.CLOUDINARY_API_KEY) {
-//     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-//     process.exist(1);
-//   }
-// }
-
-
-// if (process.env.NODE_ENV === 'production') {
-//   if (!process.env.CLOUDINARY_API_SECRET) {
-//     debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-//     process.exist(1);
-//   }
-// }
 
 app.use((err, req, res, next) => {
   if (err) {
