7298b3057d02144aa88d8377fac473cc819ebb65
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-24 16:34:13 +0100

    feat: set production function

--- production/production.js
+++ production/production.js
@@ -2,41 +2,68 @@ const debug = require('debug')('app');
 require('dotenv').config();
 
 module.exports = () => {
-
-    if (process.env.NODE_ENV === 'production') {
-        if (!process.env.CLOUDINARY_CLOUD_NAME ) {
-          debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-          process.exist(1);
-        }
-      }
-      
-      if (process.env.NODE_ENV === 'production') {
-        if (!process.env.debug ) {
-          debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-          process.exist(1);
-        }
-      }
-      
-      if (process.env.NODE_ENV === 'production') {
-        if (!process.env.MONGODBURI ) {
-          debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-          process.exist(1);
-        }
-      }
-      
-      if (process.env.NODE_ENV === 'production') {
-        if (!process.env.CLOUDINARY_API_KEY) {
-          debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-          process.exist(1);
-        }
-      }
-      
-      
-      if (process.env.NODE_ENV === 'production') {
-        if (!process.env.CLOUDINARY_API_SECRET) {
-          debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
-          process.exist(1);
-        }
-      }
-  
+  if (process.env.NODE_ENV === 'production') {
+    if (!process.env.CLOUDINARY_CLOUD_NAME) {
+      debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+      process.exist(1);
+    }
+    if (!process.env.debug) {
+      debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+      process.exist(1);
+    }
+    if (!process.env.MONGODBURI) {
+      debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+      process.exist(1);
+    }
+    if (!process.env.CLOUDINARY_API_KEY) {
+      debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+      process.exist(1);
+    }
+    if (!process.env.CLOUDINARY_API_SECRET) {
+      debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+      process.exist(1);
+    }
+  }
 };
+
+// const debug = require('debug')('app');
+// require('dotenv').config();
+
+// module.exports = () => {
+
+//     if (process.env.NODE_ENV === 'production') {
+//         if (!process.env.CLOUDINARY_CLOUD_NAME ) {
+//           debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+//           process.exist(1);
+//         }
+//       }
+
+//       if (process.env.NODE_ENV === 'production') {
+//         if (!process.env.debug ) {
+//           debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+//           process.exist(1);
+//         }
+//       }
+
+//       if (process.env.NODE_ENV === 'production') {
+//         if (!process.env.MONGODBURI ) {
+//           debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+//           process.exist(1);
+//         }
+//       }
+
+//       if (process.env.NODE_ENV === 'production') {
+//         if (!process.env.CLOUDINARY_API_KEY) {
+//           debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+//           process.exist(1);
+//         }
+//       }
+
+//       if (process.env.NODE_ENV === 'production') {
+//         if (!process.env.CLOUDINARY_API_SECRET) {
+//           debug(`FATAL ERROR: ENVIRONMENT VARIABLE NOT FOUND`);
+//           process.exist(1);
+//         }
+//       }
+
+// };
