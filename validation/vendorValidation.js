8f346ddec234afe139a9810726c0653cd3968fbe
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-07-01 17:11:18 +0100

    docs: create application code

--- validation/vendorValidation.js
+++ validation/vendorValidation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports = function validate(input) {
   const schema = Joi.object({
-    faceCapture: Joi.string(),
+    faceCapture: Joi.string().label('face capture'),
     photo: Joi.string().label('identification picture'),
     picture: Joi.string().label('business address picture'),
     business: Joi.object().keys({
@@ -29,26 +29,65 @@ module.exports = function validate(input) {
     }),
 
     personalInformation: Joi.object().keys({
-      firstname: Joi.string().required(),
-      lastname: Joi.string().required(),
-      sex: Joi.string().required().min(2).max(20).lowercase().trim().required(),
-      age: Joi.date().required(),
-      phone: Joi.string().min(7).max(15).required(),
-      email: Joi.string().email().min(3).max(500).lowercase().required(),
-      nationality: Joi.string().lowercase().trim().required(),
-      state: Joi.string().trim().required(),
-      residentialAddress: Joi.string().trim().required(),
-      city: Joi.string().trim().required(),
+      firstname: Joi.string().required().label('first name'),
+      lastname: Joi.string().required().label('last name'),
+      sex: Joi.string()
+        .required()
+        .min(2)
+        .max(20)
+        .lowercase()
+        .trim()
+        .required()
+        .label('sex'),
+      age: Joi.date().required().label('age'),
+      phone: Joi.string().min(7).max(15).required().label('phone number'),
+      email: Joi.string()
+        .email()
+        .min(3)
+        .max(500)
+        .lowercase()
+        .required()
+        .label('email'),
+      nationality: Joi.string()
+        .lowercase()
+        .trim()
+        .required()
+        .label('nationality'),
+      state: Joi.string().trim().required().label('state'),
+      residentialAddress: Joi.string()
+        .trim()
+        .required()
+        .label('residential address'),
+      city: Joi.string().trim().required().label('city'),
     }),
 
     identification: Joi.object().keys({
-      kind: Joi.string().valid('votersCard', 'intlPassport', 'nationalId'),
+      kind: Joi.string()
+        .valid('votersCard', 'intlPassport', 'nationalId')
+        .label('voterCard / intlPassport / nationalId is required'),
     }),
 
     address: Joi.object().keys({
-      location: Joi.string().min(3).max(300).lowercase().trim().required(),
-      landmark: Joi.string().min(2).max(200).lowercase().trim().required(),
-      phoneNumber: Joi.string().min(7).max(15).trim().required(),
+      location: Joi.string()
+        .min(3)
+        .max(300)
+        .lowercase()
+        .trim()
+        .required()
+        .label('location'),
+      landmark: Joi.string()
+        .min(2)
+        .max(200)
+        .lowercase()
+        .trim()
+        .required()
+        .label('landmark'),
+      phoneNumber: Joi.string()
+        .min(7)
+        .max(15)
+        .trim()
+        .required()
+        .label('phone number'),
       phoneNumberOfDirectors: Joi.string()
         .min(7)
         .max(15)
